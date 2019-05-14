bcb70259d90d62146a720b954a2dbc8ccc5789b2
cmtdmbfl <2981972702@qq.com>
2019-05-14 22:08:33 +0800

    no message

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,33 +7,33 @@
       <!-- 
       :auto-upload="false"
       :show-file-list="false"
-      :file-list="fileList"-->
+      :file-list="fileList"
+      至少需要写一个list-type用来获取file.url-->
       <el-upload
-        action="http://192.168.5.12:9090/upload1/cutImages/"
+        action="http://192.168.5.12:9090/upload1/cutImage/"
         :on-remove="handleRemove"
         ref="upload"
         :on-preview="handlePictureCardPreview"
         :on-success="handleSuccess"
         :on-error="uploadError"
         :before-upload="avatar"
-        list-type="picture"
+        list-type="text"
       >
         <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
         <el-button style="margin-left: 10px;" size="small" type="danger" @click="clearAll">清空</el-button>
-        <div slot="tip" class="el-upload__tip">只能上传jpeg/jpg文件，且不超过2Mb</div>
-        <!-- <el-button
-            style="margin-left: 10px;"
-            size="small"
-            type="success"
-            @click="submitUpload"
-        >上传到服务器</el-button>-->
+        <div slot="tip" class="el-upload__tip">只能上传jpeg/jpg文件,且不超过2Mb,仅支持单文件上传</div>
       </el-upload>
       <el-dialog :visible.sync="dialogVisible">
         <img width="100%" :src="dialogImageUrl" alt>
       </el-dialog>
     </div>
     <div class="draw">
-      <canvas id="Vas" width="1080px" height="1400px" style="border:1px solid #d3d3d3"></canvas>
+      <canvas id="Vas" width="1020px" height="1320px" style="border:1px solid #d3d3d3"></canvas>
+    </div>
+    <div class="btn">
+      <el-dialog :visible.sync="dialogB">
+        <img width="100%" :src="k" alt="">
+      </el-dialog>
     </div>
   </div>
 </template>
--- src/components/hello.js
+++ src/components/hello.js
@@ -4,10 +4,17 @@ export default {
     return {
       fileList: [],
       dialogImageUrl: "",
-      dialogVisible: false
+      dialogVisible: false,
+      k: "",
+      dialogB: false,
     };
   },
   methods: {
+    // 查看剪切图
+    saw(h) {
+      // this.dialogB = true;
+      console.log(h.id);
+    },
     // 清空
     clearAll() {
       this.$refs.upload.clearFiles();
@@ -18,13 +25,16 @@ export default {
     // 移除
     handleRemove(file, fileList) {
       // console.log(file, fileList);
+      var cd = document.getElementById("Vas");
+      var cdxt = cd.getContext("2d");
+      cdxt.clearRect(0, 0, cd.width, cd.height);
     },
-    //预览
+    // 预览,需要获取url
     handlePictureCardPreview(file) {
       this.dialogImageUrl = file.url;
       this.dialogVisible = true;
     },
-    // 上传
+    // 手动上传
     // submitUpload() {
     //   this.$refs.upload.submit();
     // },
@@ -40,98 +50,47 @@ export default {
       }
       return isJPG && isLimit;
     },
-    // 成功
+    // 上传成功
     handleSuccess(res, file) {
       console.log("上传成功");
       // console.log(res);
+      let k = res.data.listResult.length;
       // 延迟渲染
       this.$nextTick(() => {
         var c = document.getElementById("Vas");
         var ctx = c.getContext("2d");
         var img9 = new Image();
         img9.src = file.url;
-        // console.log(img9);
         img9.setAttribute("crossOrigin", "anonymous");
         img9.onload = function() {
           ctx.drawImage(img9, 0, 0);
           ctx.stroke();
         };
         //封装渲染
-        let setCanvas = (obj, i) => {
-          var { x1, y1, x2, y2 } = obj;
-          var img = new Image();
-          img.src = "data:image/png;base64," + res.data1.lsitByte[i];
-          img.setAttribute("crossOrigin", "anonymous");
-          img.onload = function() {
-            // ctx.drawImage(img, x1, y1, x2, y2);
-            ctx.moveTo(x1, y1);
-            ctx.lineTo(x1, y1 + y2);
-            ctx.lineTo(x1 + x2, y1 + y2);
-            ctx.lineTo(x1 + x2, y1);
-            ctx.lineTo(x1, y1);
-            ctx.stroke();
-          };
+        let setCanvas = obj => {
+          var { x, y, w, h } = obj;
+          // 后端给图片的base64码
+          // var img = new Image();
+          // img.src = "data:image/png;base64," + res.data.lsitByte[i];
+          // img.setAttribute("crossOrigin", "anonymous");
+          // img.onload = function() {
+          // ctx.drawImage(img, x, y, w, h);
+          ctx.moveTo(x, y);
+          ctx.lineTo(x, y + h);
+          ctx.lineTo(x + w, y + h);
+          ctx.lineTo(x + w, y);
+          ctx.lineTo(x, y);
+          ctx.stroke();
+          // };
         };
-        let xy = [
-          {
-            x1: 130,
-            y1: 140,
-            x2: 775,
-            y2: 174
-          },
-          {
-            x1: 100,
-            y1: 410,
-            x2: 140,
-            y2: 130
-          },
-          {
-            x1: 410,
-            y1: 410,
-            x2: 140,
-            y2: 130
-          },
-          {
-            x1: 710,
-            y1: 410,
-            x2: 140,
-            y2: 130
-          },
-          {
-            x1: 100,
-            y1: 590,
-            x2: 280,
-            y2: 180
-          },
-          {
-            x1: 410,
-            y1: 590,
-            x2: 280,
-            y2: 180
-          },
-          {
-            x1: 710,
-            y1: 590,
-            x2: 280,
-            y2: 180
-          },
-          {
-            x1: 100,
-            y1: 870,
-            x2: 380,
-            y2: 340
-          },
-          {
-            x1: 500,
-            y1: 870,
-            x2: 380,
-            y2: 340
-          }
-        ];
         for (let i = 0; i < 9; i++) {
-          setCanvas(xy[i], i);
+          setCanvas(res.data.listResult[i]);
         }
       });
+      for (let j = 1; j <= k; j++) {
+        this.l = j;
+        // console.log(this.l);
+      }
     },
     // 失败
     uploadError() {
@@ -139,114 +98,3 @@ export default {
     }
   }
 };
-
-//贼鸡儿繁琐的蠢方法
-// var img0 = new Image();
-// img0.src = 'data:image/png;base64,'+res.data1.lsitByte[0];
-// img0.setAttribute('crossOrigin', 'anonymous');
-// img0.onload = function() {
-//   ctx.drawImage(img0,130,140,775,174);//ctx.drawImage(img0,x1,y1,x2,y2);
-//   ctx.moveTo(130,140);                //ctx.moveTo(x1,y1);
-//   ctx.lineTo(130,174+140);            //ctx.lineTo(x1,y1+y2);
-//   ctx.lineTo(775+130,174+140);        //ctx.lineTo(x1+x2,y1+y2);
-//   ctx.lineTo(775+130,140);            //ctx.lineTo(x1+x2,y1);
-//   ctx.lineTo(130,140);                //ctx.lineTo(x1,y1);
-//   ctx.stroke();
-// };
-// console.log(img0)
-// var img1 = new Image();
-// img1.src = 'data:image/png;base64,'+res.data1.lsitByte[1];
-// img1.setAttribute('crossOrigin', 'anonymous');
-// img1.onload = function() {
-//   ctx.drawImage(img1,100,420,140,130);
-//   ctx.moveTo(100,420);
-//   ctx.lineTo(100,130+420);
-//   ctx.lineTo(140+100,130+420);
-//   ctx.lineTo(140+100,420);
-//   ctx.lineTo(100,420);
-//   ctx.stroke();
-// };
-// var img2 = new Image();
-// img2.src = 'data:image/png;base64,'+res.data1.lsitByte[2];
-// img2.setAttribute('crossOrigin', 'anonymous');
-// img2.onload = function() {
-//   ctx.drawImage(img2,420,420,140,130);
-//   ctx.moveTo(420,420);
-//   ctx.lineTo(420,130+420);
-//   ctx.lineTo(140+420,130+420);
-//   ctx.lineTo(140+420,420);
-//   ctx.lineTo(420,420);
-//   ctx.stroke();
-// };
-// var img3 = new Image();
-// img3.src = 'data:image/png;base64,'+res.data1.lsitByte[3];
-// img3.setAttribute('crossOrigin', 'anonymous');
-// img3.onload = function() {
-//   ctx.drawImage(img3,720,420,140,130);
-//   ctx.moveTo(720,420);
-//   ctx.lineTo(720,130+420);
-//   ctx.lineTo(140+720,130+420);
-//   ctx.lineTo(140+720,420);
-//   ctx.lineTo(720,420);
-//   ctx.stroke();
-// };
-// var img4 = new Image();
-// img4.src = 'data:image/png;base64,'+res.data1.lsitByte[4];
-// img4.setAttribute('crossOrigin', 'anonymous');
-// img4.onload = function() {
-//   ctx.drawImage(img4,100,590,280,180);
-//   ctx.moveTo(100,590);
-//   ctx.lineTo(100,180+590);
-//   ctx.lineTo(280+100,180+590);
-//   ctx.lineTo(280+100,590);
-//   ctx.lineTo(100,590);
-//   ctx.stroke();
-// };
-// var img5 = new Image();
-// img5.src = 'data:image/png;base64,'+res.data1.lsitByte[5];
-// img5.setAttribute('crossOrigin', 'anonymous');
-// img5.onload = function() {
-//   ctx.drawImage(img5,420,590,280,180);
-//   ctx.moveTo(420,590);
-//   ctx.lineTo(420,180+590);
-//   ctx.lineTo(280+420,180+590);
-//   ctx.lineTo(280+420,590);
-//   ctx.lineTo(420,590);
-//   ctx.stroke();
-// };
-// var img6 = new Image();
-// img6.src = 'data:image/png;base64,'+res.data1.lsitByte[6];
-// img6.setAttribute('crossOrigin', 'anonymous');
-// img6.onload = function() {
-//   ctx.drawImage(img6,720,590,280,180);
-//   ctx.moveTo(720,590);
-//   ctx.lineTo(720,180+590);
-//   ctx.lineTo(280+720,180+590);
-//   ctx.lineTo(280+720,590);
-//   ctx.lineTo(720,590);
-//   ctx.stroke();
-// };
-// var img7 = new Image();
-// img7.src = 'data:image/png;base64,'+res.data1.lsitByte[7];
-// img7.setAttribute('crossOrigin', 'anonymous');
-// img7.onload = function() {
-//   ctx.drawImage(img7,100,870,380,340);
-//   ctx.moveTo(100,870);
-//   ctx.lineTo(100,340+870);
-//   ctx.lineTo(380+100,340+870);
-//   ctx.lineTo(380+100,870);
-//   ctx.lineTo(100,870);
-//   ctx.stroke();
-// };
-// var img8 = new Image();
-// img8.src = 'data:image/png;base64,'+res.data1.lsitByte[8];
-// img8.setAttribute('crossOrigin', 'anonymous');
-// img8.onload = function() {
-//   ctx.drawImage(img8,500,870,380,340);
-//   ctx.moveTo(500,870);
-//   ctx.lineTo(500,340+870);
-//   ctx.lineTo(380+500,340+870);
-//   ctx.lineTo(380+500,870);
-//   ctx.lineTo(500,870);
-//   ctx.stroke();
-// };
